52b4a2320b2f560df7cba889e8f5a9003a6ae29e
hoholms <ninikabug@gmail.com>
2026-03-10 02:57:07 +0200

    Initial commit

---
+++ src/main/java/com/meradeya/app/MeradeyaApplication.java
@@ -0,0 +1,13 @@
+package com.meradeya.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeradeyaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MeradeyaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/meradeya/app/MeradeyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meradeya.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeradeyaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
